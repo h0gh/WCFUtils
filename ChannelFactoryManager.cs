a36816b159e66d2a047736855c00de922965c57f
jeh <jeh@miracleas.dk>
2013-12-12 14:28:50 +0100

    Add channelfactorymanager and wcfserviceinvoker

---
+++ ChannelFactoryManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace WCFUtils
+{
+    
+    /// <summary>
+    /// Adapted from Darin Dimitrov http://stackoverflow.com/questions/3200197/creating-wcf-channelfactoryt/3201001#3201001
+    /// </summary>
+    public sealed class ChannelFactoryManager : IDisposable
+    {
+        private readonly Dictionary<Type, ChannelFactory> _factories = new Dictionary<Type, ChannelFactory>();
+        private readonly object _syncRoot = new object();
+
+        public T CreateChannel<T>() where T : class
+        {
+            return CreateChannel<T>("*", null);
+        }
+
+        public T CreateChannel<T>(string endpointConfigurationName) where T : class
+        {
+            return CreateChannel<T>(endpointConfigurationName, null);
+        }
+
+        public T CreateChannel<T>(string endpointConfigurationName, string endpointAddress) where T : class
+        {
+            T local = GetFactory<T>(endpointConfigurationName, endpointAddress).CreateChannel();
+            ((IClientChannel)local).Faulted += ChannelFaulted;
+            return local;
+        }
+
+        private ChannelFactory<T> GetFactory<T>(string endpointConfigurationName, string endpointAddress) where T : class
+        {
+            lock (_syncRoot)
+            {
+                ChannelFactory factory;
+                if (!_factories.TryGetValue(typeof(T), out factory))
+                {
+                    factory = CreateFactoryInstance<T>(endpointConfigurationName, endpointAddress);
+                    _factories.Add(typeof(T), factory);
+                }
+                return (factory as ChannelFactory<T>);
+            }
+        }
+
+        private ChannelFactory CreateFactoryInstance<T>(string endpointConfigurationName, string endpointAddress)
+        {
+            ChannelFactory factory = string.IsNullOrEmpty(endpointAddress)
+                ? new ChannelFactory<T>(endpointConfigurationName)
+                : new ChannelFactory<T>(endpointConfigurationName, new EndpointAddress(endpointAddress));
+            factory.Faulted += FactoryFaulted;
+            factory.Open();
+            return factory;
+        }
+
+        private void ChannelFaulted(object sender, EventArgs e)
+        {
+            var channel = (IClientChannel)sender;
+            try
+            {
+                channel.Close();
+            }
+            catch
+            {
+                channel.Abort();
+            }
+            throw new ApplicationException("Exc_ChannelFailure");
+        }
+
+        private void FactoryFaulted(object sender, EventArgs args)
+        {
+            var factory = (ChannelFactory)sender;
+            try
+            {
+                factory.Close();
+            }
+            catch
+            {
+                factory.Abort();
+            }
+            Type[] genericArguments = factory.GetType().GetGenericArguments();
+            if (genericArguments.Length == 1)
+            {
+                Type key = genericArguments[0];
+                if (_factories.ContainsKey(key))
+                {
+                    _factories.Remove(key);
+                }
+            }
+            throw new ApplicationException("Exc_ChannelFactoryFailure");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+
+            lock (_syncRoot)
+            {
+                foreach (Type type in _factories.Keys)
+                {
+                    ChannelFactory factory = _factories[type];
+                    try
+                    {
+                        factory.Close();
+                    }
+                    catch
+                    {
+                        factory.Abort();
+                    }
+                }
+                _factories.Clear();
+            }
+        }
+    }
+}
---
+++ WCFServiceInvoker.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.ServiceModel;
+using System.ServiceModel.Configuration;
+
+namespace WCFUtils
+{
+    /// <summary>
+    /// Adapted from Darin Dimitrov http://stackoverflow.com/questions/3200197/creating-wcf-channelfactoryt/3201001#3201001
+    /// </summary>
+    public class WCFServiceInvoker
+    {
+        private readonly ChannelFactoryManager _factoryManager;
+        private static readonly ClientSection _clientSection = ConfigurationManager.GetSection("system.serviceModel/client") as ClientSection;
+
+        public WCFServiceInvoker(ChannelFactoryManager factoryManager)
+        {
+            _factoryManager = factoryManager;
+        }
+
+        public TR InvokeService<T, TR>(Func<T, TR> invokeHandler) where T : class
+        {
+            var endpointNameAddressPair = GetEndpointNameAddressPair(typeof(T));
+            var arg = _factoryManager.CreateChannel<T>(endpointNameAddressPair.Key, endpointNameAddressPair.Value);
+            var communicationObject = (ICommunicationObject)arg;
+            try
+            {
+                return invokeHandler(arg);
+            }
+            finally
+            {
+                DoTheShutdownDance(communicationObject);
+            }
+        }
+
+        public void InvokeService<T>(Action<T> invokeHandler) where T : class
+        {
+            var endpointNameAddressPair = GetEndpointNameAddressPair(typeof(T));
+            var arg = _factoryManager.CreateChannel<T>(endpointNameAddressPair.Key, endpointNameAddressPair.Value);
+            var communicationObject = (ICommunicationObject)arg;
+            try
+            {
+                invokeHandler(arg);
+            }
+            finally
+            {
+                DoTheShutdownDance(communicationObject);
+            }
+        }
+
+        private static void DoTheShutdownDance(ICommunicationObject communicationObject)
+        {
+            try
+            {
+                if (communicationObject.State != CommunicationState.Faulted)
+                {
+                    communicationObject.Close();
+                }
+            }
+            catch
+            {
+                communicationObject.Abort();
+            }
+        }
+
+        private KeyValuePair<string, string> GetEndpointNameAddressPair(Type serviceContractType)
+        {
+            var configException = new ConfigurationErrorsException(string.Format("No client endpoint found for type {0}. Please add the section <client><endpoint name=\"myservice\" address=\"http://address/\" binding=\"basicHttpBinding\" contract=\"{0}\"/></client> in the config file.", serviceContractType));
+            if (((_clientSection == null) || (_clientSection.Endpoints == null)) || (_clientSection.Endpoints.Count < 1))
+            {
+                throw configException;
+            }
+            foreach (ChannelEndpointElement element in _clientSection.Endpoints)
+            {
+                if (element.Contract == serviceContractType.ToString())
+                {
+                    return new KeyValuePair<string, string>(element.Name, element.Address.AbsoluteUri);
+                }
+            }
+            throw configException;
+        }
+    }
+}
